aa3530cbe52da3f101862bd3268b16a8e9208f09
64J0 <vinigaio97@gmail.com>
2020-08-01 20:47:07 -0300

    Atualizando as meta tags

--- nextjs-blog/pages/_document.js
+++ nextjs-blog/pages/_document.js
@@ -1,7 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-const siteTitle = "64j0 Blog";
-
 class MyDocument extends Document {
   render() {
     return (
@@ -13,7 +11,12 @@ class MyDocument extends Document {
             name="description"
             content="Blog pessoal onde pretendo compartilhar algumas coisas que estou estudando."
           />
-          <meta name="og:title" content={siteTitle} />
+          <meta name="og:title" content="64j0 Blog" />
+          <meta name="og:site_name" content="64j0 blog" />
+          <meta name="og:image" content="/favicon.ico" />
+          <meta name="og:locale" content="pt_BR" />
+          <meta name="og:url" content="https://gajo.vercel.app" />
+          <meta name="og:type" content="website" />
         </Head>
         <body>
           <Main />
