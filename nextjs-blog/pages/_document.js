a2f9c5c71b93a599d9781773be52381962e87b16
gajo <vinigaio97@gmail.com>
2020-11-22 14:19:26 -0300

    Atualizando o og:image

--- nextjs-blog/pages/_document.js
+++ nextjs-blog/pages/_document.js
@@ -16,7 +16,7 @@ class MyDocument extends Document {
           />
           <meta name="og:title" content="64j0 Blog" />
           <meta name="og:site_name" content="64j0 blog" />
-          <meta name="og:image" content="/favicon.ico" />
+          <meta name="og:image" content="https://avatars1.githubusercontent.com/u/50725287?s=460&u=a543b28cd2cae2b76fdc3cd4ea1699c35b7b7f06&v=4" />
           <meta name="og:locale" content="pt_BR" />
           <meta name="og:url" content="https://gajo.vercel.app" />
           <meta name="og:type" content="website" />
