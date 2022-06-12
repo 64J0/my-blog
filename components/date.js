fbd869cbeba574e0edf8262aaffbf629bc8c23ed
64J0 <vinigaio97@gmail.com>
2022-06-12 10:13:35 -0300

    chore: update config due to ts

--- components/date.js
+++ components/date.js
@@ -1,12 +1,13 @@
+import React from "react";
 import { parseISO, format } from "date-fns";
 
-import { ptBR } from 'date-fns/locale';
+import { ptBR } from "date-fns/locale";
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString);
   return (
     <time dateTime={dateString}>
-      Postado em {date ? format(date, `d ' de ' LLLL ' de ' yyyy`, { locale: ptBR }) : 'Não definido'}
+      Postado em {date ? format(date, `d " de " LLLL " de " yyyy`, { locale: ptBR }) : "Não definido"}
     </time>
   );
 }
