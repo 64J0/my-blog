fe7e31eab8a46ba0eedd6b4c293fe89f484db4e4
64J0 <vinigaio97@gmail.com>
2022-08-06 10:26:57 -0300

    fix: clear this project removing unused resources

--- pages/api/hello.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-// They can be deployed as Serverless Functions (also known as Lambdas)
-const apiTest =  (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ text: "Hello World" });
-};
-
-export default apiTest;
