f3e90bba4b82256150d3acf59d6783d5aeec3d2c
64J0 <vinigaio97@gmail.com>
2022-10-23 15:41:17 -0300

    fix: lint rules

--- components/MicrosoftClarity/index.tsx
+++ components/MicrosoftClarity/index.tsx
@@ -6,16 +6,17 @@ import Script from "next/script";
 const MicrosoftClarity: React.FC = () => {
   return (
     <Script
-        strategy="beforeInteractive"
-        dangerouslySetInnerHTML={{
-            __html: `
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "e7nckw1nc5");
-            `
-        }}>
+      id="microsoft-clarity"
+      strategy="beforeInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "e7nckw1nc5");
+        `
+      }}>
     </Script>
   );
 };
