ee941b6d60f397d2d7e0871dea71e041597a01b7
64J0 <vinigaio97@gmail.com>
2022-11-06 16:42:32 -0300

    chore: rename html -> remarkHtml

--- lib/posts.ts
+++ lib/posts.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import { remark } from "remark";
-import html from "remark-html";
+import remarkHtml from "remark-html";
 import remarkGfm from "remark-gfm";
 
 const postsDirectory = path.join(process.cwd(), "posts");
@@ -68,7 +68,7 @@ export async function getPostData(filename?: string) {
 
   // Use remark to convert markdown into HTML string
   const processedContent = await remark()
-    .use(html)
+    .use(remarkHtml)
     .use(remarkGfm)
     .process(matterResult.content);
   const contentHtml = processedContent.toString();
