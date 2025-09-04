542aab180a501d0c2c6107ee0e5a2cbd3b6ab49d
64J0 <vinigaio97@gmail.com>
2025-09-03 21:30:28 -0300

    add adsense meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,6 +25,9 @@ class MyDocument extends Document {
           <GoogleAnalytics />
           <MicrosoftClarity />
 
+          {/* Google AdSense */}
+          <meta name="google-adsense-account" content="ca-pub-1710724306210780"></meta>
+
           {/* For LaTeX/KaTeX styles */}
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossOrigin="anonymous"></link>
         </Head>
