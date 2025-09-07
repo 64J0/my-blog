e066bb63bc5100db1b7769e65a980fdb1033e88c
64J0 <vinigaio97@gmail.com>
2025-09-07 18:56:24 -0300

    use Content-Type: application/rss+xml for rss route

--- pages/rss.tsx
+++ pages/rss.tsx
@@ -7,8 +7,8 @@ const Rss: React.FC = () => null;
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   if (res) {
     const rssData = getRssData();
-    // res.setHeader('Content-Type', 'application/rss+xml');
-    res.setHeader('Content-Type', 'text/xml');
+    res.setHeader('Content-Type', 'application/rss+xml');
+    // res.setHeader('Content-Type', 'text/xml');
     res.write(rssData);
     res.end();
   }
