916a56d24bed0a05cb973b74bdace155b9e2f7a1
64J0 <vinigaio97@gmail.com>
2025-09-07 18:37:50 -0300

    improve 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import Layout from "../components/Layout";
 
 // pages/404.js
 const Custom404: React.FC = () => {
-  return <h1>404 - Página não encontrada</h1>;
+  return (
+    <Layout>
+      {/* TODO: Move style to another css file */}
+      <div style={{ textAlign: "center", padding: "4rem" }}>
+        <h1>404 - Página não encontrada</h1>
+      </div>
+    </Layout>
+  );
 };
 
 export default Custom404;
