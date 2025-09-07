ded89a53721155246d68625575ba3ee7a54efc8c
64J0 <vinigaio97@gmail.com>
2025-09-07 18:15:18 -0300

    update pages to denote that the blog is using en-US language

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,18 +7,18 @@ import MicrosoftClarity from "../components/MicrosoftClarity";
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="pt-BR">
+      <Html lang="en-US">
         <Head>
           <link rel="icon" href="/favicon.ico" />
 
           <meta
             name="description"
-            content="Blog pessoal onde pretendo compartilhar algumas coisas que estou estudando."
+            content="Personal blog where I write about technology, programming, philosophy, theology, and more."
           />
           <meta name="og:title" content="64j0 Blog" />
           <meta name="og:site_name" content="64j0 blog" />
           <meta name="og:image" content="https://avatars1.githubusercontent.com/u/50725287?s=460&u=a543b28cd2cae2b76fdc3cd4ea1699c35b7b7f06&v=4" />
-          <meta name="og:locale" content="pt_BR" />
+          <meta name="og:locale" content="en_US" />
           <meta name="og:url" content="https://gajo.vercel.app" />
           <meta name="og:type" content="website" />
 
