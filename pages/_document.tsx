ecce5c3674278bb154b3493143afa81725cebacb
64J0 <vinigaio97@gmail.com>
2025-09-07 14:51:51 -0300

    add google automatic ads script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,9 +27,18 @@ class MyDocument extends Document {
 
           {/* Google AdSense */}
           <meta name="google-adsense-account" content="ca-pub-1710724306210780"></meta>
+          <script async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1710724306210780"
+            crossOrigin="anonymous">
+          </script>
 
           {/* For LaTeX/KaTeX styles */}
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css" integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC" crossOrigin="anonymous"></link>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css"
+            integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC"
+            crossOrigin="anonymous">
+          </link>
         </Head>
         <body>
           <Main />
